86859f0ac570b9109372f6fc4a6690df0e486151
Sergey Bilous <bill-gh@yandex.ru>
2020-03-05 10:12:42 +0300

    Mover - all moves  done. Carrier schema attached

--- writer/src/main/java/ru/home/post/writer/config/Beans.java
+++ writer/src/main/java/ru/home/post/writer/config/Beans.java
@@ -1,8 +1,10 @@
 package ru.home.post.writer.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.core.env.Environment;
 import ru.home.post.writer.CreateMoves;
 import ru.home.post.writer.CreatePackages;
 import ru.home.post.writer.Initializer;
@@ -12,15 +14,17 @@ import java.io.IOException;
 @Lazy
 @Configuration
 public class Beans {
+    @Autowired
+    Environment environment;
     @Bean("createPackages")
     public CreatePackages createPackages() {
-        CreatePackages creator = new CreatePackages(25);
+        CreatePackages creator = new CreatePackages(Integer.valueOf(environment.getProperty("days.number")));
         return creator;
     }
 
     @Bean("createMoves")
     public CreateMoves createMoves() {
-        CreateMoves creator = new CreateMoves(25);
+        CreateMoves creator = new CreateMoves(Integer.valueOf(environment.getProperty("days.number")));
 
         return creator;
     }
